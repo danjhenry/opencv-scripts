6b7e2980a3a351b09d26b08cb75249a9e3134b1f
Daniel Henry <email378637@gmail.com>
2017-06-15 12:35:58 -0700

    Create color2grayscale.py

--- color2grayscale.py
+++ color2grayscale.py
@@ -3,13 +3,11 @@ import os, sys
 import numpy as np
 
 def main():
-    images = os.listdir('images')
-    images.remove('desktop.ini')
+    images = os.listdir('dir name')
     for image in images:
         path = 'images/' + image
         img = cv2.imread(path, 0)
-        newPath = 'Gray/' + image
+        newPath = 'gray/' + image
         cv2.imwrite(newPath, img)
-    print('done')
-        
+    print('done')      
 main()
