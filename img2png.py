25a051ada52d11c93da16284d3ea2b22f84ee66e
Daniel Henry <email378637@gmail.com>
2017-06-16 14:25:09 -0700

    Add files via upload

---
+++ img2png.py
@@ -0,0 +1,15 @@
+import cv2
+import os, sys
+import numpy as np
+
+def main():
+    images = os.listdir('image path')
+    for image in images:
+        path = 'image path' + image
+        img = cv2.imread(path, 3)
+        image = image[:-3]
+        newPath = 'png/' + image + 'png'
+        cv2.imwrite(newPath, img)
+    print('done')
+        
+main()
